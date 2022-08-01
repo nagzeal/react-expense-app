a29d3d74fc4a61edc1e17c024509d386071e4360
Nag Menasigi <34202071+nagzeal@users.noreply.github.com>
2022-08-01 18:58:16 +0530

    Initial Commit

---
+++ src/components/Expenses/Expenseitems.js
@@ -0,0 +1,23 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./Expenseitems.css";
+
+const Expenseitems = (props) => {
+  
+  const clickHandler = () => {
+    console.log("Clicked");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
+
+export default Expenseitems;
